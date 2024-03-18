791760bc83f91360ee0646c44149192d9555f44f
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-03-18 15:48:31 +0500

    added slider, wagon and particles

---
+++ Assets/Scripts/Environments/MotorSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Character.Classes;
+using UnityEngine;
+
+namespace Environments
+{
+    [RequireComponent(typeof(SliderJoint2D))]
+    public class MotorSwitcher : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private float _delay;
+        
+        private SliderJoint2D _slider;
+        private void Start() => _slider = GetComponent<SliderJoint2D>();
+
+        public void Interact()
+        {
+            var motor = _slider.motor;
+            motor.motorSpeed *= -1;
+            _slider.motor = motor;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.gameObject.TryGetComponent(out Person _)) StartCoroutine(Delay());
+        }
+
+        private IEnumerator Delay()
+        {
+            yield return new WaitForSeconds(_delay);
+            Interact();
+        }
+    }
+}
